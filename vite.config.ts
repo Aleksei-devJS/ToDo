531904784feaed57b3891379e61a4e3d73eb8e51
Aleksei-devJS <aleksei.voroshiloff@gmail.com>
2025-03-28 17:28:46 +0300

    v 2.2.1

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
-    assetsDir: "assets",
     emptyOutDir: true,
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        entryFileNames: "assets/[name]-[hash].js",
-      },
-    },
   },
 });
